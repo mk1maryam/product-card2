7f43458e0191db17f8e1fcf84f409dfe219560a8
maryam <lumi.101316@gmail.com>
2026-09-24 16:44:03 +0300

    Закреплены базовые навыки работы с переменными, функциями и условиями

--- homework-7.js
+++ homework-7.js
@@ -1,38 +1,48 @@
+function printWeather(cityName, temperature) {
+  console.log(
+    "Сегодня в " + cityName + " температура " + temperature + " градусов",
+  );
+}
+printWeather("Каир", 35);
+printWeather("Бишкек", 28);
 
+const speed_of_light = 299792458;
+function checkSpeed(Speed) {
+  if (Speed > speed_of_light) {
+    console.log("Скорость превышает скорость света");
+  } else if (Speed < speed_of_light) {
+    console.log("Световая скорость");
+  } else {
+    console.log("Скорость света");
+  }
+}
+checkSpeed(233792458);
+checkSpeed(300045889);
+checkSpeed(299792458);
 
-function printWeather(cityName, temperature ) {
-    console.log("Сегодня в "+cityName+" температура "+temperature+" градусов");}
-    printWeather("Каир", 35);
-    printWeather("Бишкек", 28);
+const productName = "Набор косметики";
+const productPrice = 1500;
 
-    const speed_of_light=299792458;
-    function chevkSpeed(currentSpeed) {
-        if (currentSpeed>speed_of_light) {
-            console.log("Скорость превышает скорость света");
-        }
-        else if (currentSpeed===speed_of_light) {
-            console.log("Скорость равна скорости света");
-        }
-        checkSpeed(233792458);
-        checkSpeed(300045889);
-        checkSpeed(299792458);
-    }
+function buyProduct(Budget) {
+  if (Budget >= productPrice) {
+    console.log(`"${productName}" куплен, спасибо за покупку!`);
+  } else {
+    const difference = productPrice - Budget;
+    console.log(
+      `Вам не хватает средств для покупки ${productName}, ${difference} ₽, пополните баланс.`,
+    );
+  }
+}
+buyProduct(2000);
+buyProduct(1000);
+const budget = 2000;
+if (budget >= productPrice) {
+  console.log(`"${productName}" куплен, спасибо за покупку!`);
+}
+buyProduct(1000);
 
-    const productName="Набор косметики";
-    const productPrice=1500;
-    console.log("Название продукта: "+productName+", Цена: "+productPrice+" ₽");
-    function checkBudget(userBudget) {
-        if (userBudget>=productPrice) {
-            console.log('"рубль"{productName} приобретен. Спасибо за покупку!');
-        }
-        else {
-            const missingAmount=productPrice-userBudget;
-            console.log("Недостаточно средств. Не хватает "+missingAmount+" ₽ для покупки "+productName);
-        }
-    }
-    checkBudget(2000);
-    checkBudget(1000);
+function printUserInfo(name, age, city) {
+  console.log("Меня зовут " + name + ", мне " + age + " лет, я живу в " + city);
+}
 
-    const myName="Марьям";
-var myAge=40;
-console.log("Меня зовут "+myName+", мне "+myAge+" лет");
+printUserInfo("Марьям", 40, "Бишкек");
